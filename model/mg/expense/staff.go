2996ce9849e2f376354e7ec15de4edf1002abbc9
tuannt20 <105765641+tuannt20@users.noreply.github.com>
2023-01-31 09:24:32 +0700

    :art: Remove field
    Remove field

--- model/mg/expense/staff.go
+++ model/mg/expense/staff.go
@@ -13,7 +13,6 @@ type Staff struct {
 	Password     string    `bson:"password"`
 	Code         string    `bson:"code"`
 	Status       string    `bson:"status"`
-	IsRoot       bool      `bson:"isRoot"` // IsRoot=true -> Admin
 	CreatedAt    time.Time `bson:"createdAt"`
 	UpdatedAt    time.Time `bson:"updatedAt"`
 }
