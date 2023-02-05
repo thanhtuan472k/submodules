7720f8dc019b569560305c7b33183ff06708510d
tuannt20 <105765641+tuannt20@users.noreply.github.com>
2023-02-05 10:17:29 +0700

    :art: Add func generate token
    Add func generate token

--- model/mg/expense/staff.go
+++ model/mg/expense/staff.go
@@ -1,6 +1,9 @@
 package expense
 
-import "time"
+import (
+	"github.com/golang-jwt/jwt"
+	"time"
+)
 
 // Staff ...
 type Staff struct {
@@ -14,4 +17,21 @@ type Staff struct {
 	Status       string    `bson:"status"`
 	CreatedAt    time.Time `bson:"createdAt"`
 	UpdatedAt    time.Time `bson:"updatedAt"`
+}
+
+// GenerateAccessToken ...
+func (s Staff) GenerateAccessToken() string {
+	// Init claims
+	claims := jwt.MapClaims{
+
+		"_id":  s.ID,
+		"name": s.Name,
+		"exp":  time.Now().Local().Add(time.Second * 15552000).Unix(), // 6 months
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	tokenString, _ := token.SignedString([]byte("AuthSecret"))
+
+	return tokenString
 }
