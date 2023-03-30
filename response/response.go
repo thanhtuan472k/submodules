3b227eda9b0f1bcfa7673eaa9967dcb668f562c0
tuannt20 <tuango.472000@gmail.com>
2023-03-30 11:20:17 +0700

    :recycle: refactor: Using common struct response for admin & admin
    refactor: Using common struct response for admin & admin

--- response/response.go
+++ response/response.go
@@ -1,1 +1,11 @@
 package response
+
+// ResponseCreate ...
+type ResponseCreate struct {
+	ID string `json:"_id"`
+}
+
+// ResponseUpdate ...
+type ResponseUpdate struct {
+	ID string `json:"_id"`
+}
